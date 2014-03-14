dbac0c00de2324a4ea775e991a5e148c15b2dca4
Mession <messionyk@gmail.com>
2014-03-14 09:44:46 +0200

    index.php pieni muokkaus

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<?php 
+  $index = "Hearthstone deck builder"; 
+?><!DOCTYPE HTML>
+<html>
+<head><title><?php echo $index; ?></title></head>
+<body>
+  <h1><?php echo $index; ?></h1>
+</body>
+</html>
