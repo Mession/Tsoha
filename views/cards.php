6f08f20da6fc45255e7b932d01c2781d85656ff8
Mession <messionyk@gmail.com>
2014-04-05 05:18:28 +0300

    korttilistan filtterointi mahdollistettu

--- views/cards.php
+++ views/cards.php
@@ -1,5 +1,27 @@
+<script type="text/javascript" src="js/jquery-2.1.0.js"></script>
+<script>$(document).ready(function () {
+
+    (function ($) {
+
+        $('#filter').keyup(function () {
+
+            var rex = new RegExp($(this).val(), 'i');
+            $('.searchable tr').hide();
+            $('.searchable tr').filter(function () {
+                return rex.test($(this).text());
+            }).show();
+
+        })
+
+    }(jQuery));
+
+});
+</script>
 <div class="container">
     <h1>List of cards</h1>
+    <div class="input-group">
+        <input id="filter" type="text" class="form-control" placeholder="Filter results">
+    </div>
     <table class="table table-striped">
         <thead>
             <tr>
@@ -7,7 +29,7 @@
                 <th>Class</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody class="searchable">
             <?php foreach ($data->cards as $card): ?>
                 <tr>
                     <td>
